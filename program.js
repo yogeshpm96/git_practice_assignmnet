04856dbfdd5bb67d12af8c1f9ebf51524f630127
yogeshpm96 <yogeshmahajan500@gmail.com>
2022-11-30 14:58:56 +0530

    code_is_created

---
+++ program.js
@@ -0,0 +1,18 @@
+let str="naman";
+let N=5;
+let bag="";
+
+for(let i=N-1; i>=0; i--)
+  {
+
+    bag=baaag+str[i];
+  }
+
+if(str==bag)
+{
+  console.log("String is Palindrome");
+}
+else
+{
+  console.log("String is Not Palindrome");
+}
